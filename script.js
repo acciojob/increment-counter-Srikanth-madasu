4edf22dc5e5d0cdd6cf2fe482b470e48c03439db
Srikanth-madasu <142045491+Srikanth-madasu@users.noreply.github.com>
2024-03-25 16:42:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+let counter =document.getElementById("counter");
+let btn =document.getElementById("incrementBtn");
+
+btn.onclick=()=>{
+	alert(counter.innerText);
+	counter.innerText++;
+}
